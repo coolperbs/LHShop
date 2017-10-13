089ee71f2d926ad3d64ed9b863cd44a09876f0b9
jun.li <jun.li@dmall.com>
2017-10-13 14:20:18 +0800

    完成分类

--- views/category/category.js
+++ views/category/category.js
@@ -4,23 +4,47 @@ var handle, events, _fn,
     app = getApp();
 
 handle = {
-  render : function( callerPage ) {
+  render : function( callerPage, options ) {
     _fn.init( callerPage );
+
+    if ( options.type == 'show' && callerPage.data && callerPage.data.viewData && callerPage.data.viewData.currentTab ) {
+      return;
+    }
     // 获取购物车数据
     _fn.getViewData( function( res ) {
-      if ( utils.isErrorRes( res ) ) {
-        return;
-      }
-      console.log( res );
-      callerPage.setData( {
+      var data = callerPage.data,
+          newData = {
         'viewData.cat' : res.data
+      };
+      //if ( options.type == 'changeTab' ) {
+        newData['viewData.currentTab'] = res.data[0]
+      //} else if ( options.type == 'show' ) {
+      //  newData['viewData.currentTab'] = ( data && data.viewData && data.viewData.currentTab ) ? data.viewData.currentTab : res.data[0]
+      //}
+      callerPage.setData( {
+        'viewData.cat' : res.data,
+        'viewData.currentTab' : res.data[0]
       } );
     } );
   }
 };
 
 events = {
-  goCheck : function( e ) {
+  changeTab : function( e ) {
+    var dataset = e.currentTarget.dataset,
+        callerPage = this,
+        viewData = callerPage.data.viewData,
+        id = dataset.id,
+        i, len;
+
+    for ( i = 0, len = viewData.cat.length; i < len; ++i ) {
+      if ( id == viewData.cat[i].catId ) {
+        callerPage.setData( {
+          'viewData.currentTab' : viewData.cat[i]
+        } );
+        return;
+      }
+    }
     // 判断选中态等情况
     //wx.navigateTo( { url : '../checkout/checkout?cartid=' + e.currentTarget.dataset.cartid } );
   }
@@ -28,18 +52,19 @@ events = {
 
 _fn = {
   init : function( callerPage ) {
-    if ( callerPage.initedCart ) {
+    if ( callerPage.initedCategory ) {
       return;
     }
     utils.mix( callerPage, {
       categoryClickProxy : function( e ) {
         var target = e.currentTarget;
         if ( target.dataset && target.dataset.fn && events[target.dataset.fn] ) {
+        console.log( e );
           events[target.dataset.fn].call( this, e );
         }
       }
     } );
-    callerPage.initedCart = true;
+    callerPage.initedCategory = true;
   },
   getViewData : function( callback ) {
     ajax.query( {
